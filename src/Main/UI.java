f73daf57856f07730b674979c63aa8327377167f
superultramike11 <tuj25271@temple.edu>
2023-03-28 00:19:57 -0400

    Initial commit

---
+++ src/Main/UI.java
@@ -0,0 +1,55 @@
+package Main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UI {
+    GameManager gm;
+    JFrame window;
+    public JTextArea messageText;
+    public JPanel bgPanel[] = new JPanel[10];
+    public JLabel bgLabel[] = new JLabel[10];
+
+    public UI(GameManager gm) {
+        this.gm = gm;
+
+        createMainField();
+        createBackground();
+
+        window.setVisible(true);
+    }
+
+    public void createMainField() {
+        window = new JFrame();
+        window.setSize(800, 600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().setBackground(Color.black);
+        window.setLayout(null);
+
+        messageText = new JTextArea("THIS IS SAMPLE TEXT");
+        messageText.setBounds(50,410,700,150);
+        messageText.setBackground(Color.black);
+        messageText.setForeground(Color.white);
+        messageText.setEditable(false);
+        messageText.setLineWrap(true);
+        messageText.setWrapStyleWord(true);
+        messageText.setFont(new Font("Book Antiqua", Font.PLAIN, 26));
+        window.add(messageText);
+    }
+
+    public void createBackground() {
+        bgPanel[1] = new JPanel();
+        bgPanel[1].setBounds(50, 50, 700, 350);
+        bgPanel[1].setBackground(Color.blue);
+        bgPanel[1].setLayout(null);
+        window.add(bgPanel[1]);
+
+        bgLabel[1] = new JLabel();
+        bgLabel[1].setBounds(0, 0, 700, 350);
+
+        ImageIcon bgIcon = new ImageIcon(getClass().getClassLoader().getResource("greenbg700x350.png"));
+        bgLabel[1].setIcon(bgIcon);
+
+        bgPanel[1].add(bgLabel[1]);
+    }
+}
